9300f0f69b737d19792d54c9cdde8ba62919a177
Ben <benkatan17@gmail.com>
2018-08-18 13:17:14 +0300

    adding 2 pages

--- src/app/components/black-rain/black-rain.component.ts
+++ src/app/components/black-rain/black-rain.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-black-rain',
   templateUrl: './black-rain.component.html',
-  styleUrls: ['./black-rain.component.css']
+  styleUrls: ['./black-rain.component.scss']
 })
 export class BlackRainComponent implements OnInit {
 
--- src/app/components/evrey-one-wants-to-live/evrey-one-wants-to-live.component.ts
+++ src/app/components/evrey-one-wants-to-live/evrey-one-wants-to-live.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import AOS from '../../../assets/jsFile/aos.js';
 
 @Component({
   selector: 'app-evrey-one-wants-to-live',
   templateUrl: './evrey-one-wants-to-live.component.html',
-  styleUrls: ['./evrey-one-wants-to-live.component.css']
+  styleUrls: ['./evrey-one-wants-to-live.component.scss']
 })
 export class EvreyOneWantsToLiveComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+   AOS.init();
   }
 
 }
